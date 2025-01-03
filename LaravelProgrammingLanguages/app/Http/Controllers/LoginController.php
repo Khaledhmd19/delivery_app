71965714e846dfe05308acf9b6fe4f1fdff82c19
Hamzeh_Miqdad <hmozeh2@gmail.com>
2025-01-03 22:50:26 +0300

    added files

---
+++ LaravelProgrammingLanguages/app/Http/Controllers/LoginController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rules\Isnumber;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+class LoginController extends Controller
+{
+    
+    public function store(){
+        if (Auth::check()) {
+            return response()->json(['message' => 'Access denied for authenticated users.'], 403);
+        }
+        request()->validate([
+            'otp'=> ['min:4','max:4'],
+            'number'=>['min:9','max:10',new Isnumber()]
+        ]);
+        $number = request('number');
+        if(strlen($number)==10){
+            $number= substr($number,1);
+        }
+        $number = '+963'. $number;
+
+        $user =User::where('number','=',$number)->first();
+        if(!$user){
+            return response()->json([
+                'verified'=>false,
+                'time'=>false,
+                'message' => 'Number is not correct'
+            ],400);
+        }
+        if(Carbon::now() > $user->otp_timestamp){
+                return response()->json([
+                'verified'=>false,
+                'time'=>false,
+                'message' => 'The verification code has expired. Please request a new code.'
+                ],400);
+        }
+        if($user->otp != request('otp')){
+            return response()->json([
+                'verified'=>false,
+                'time'=>true,
+                'message' => 'Invalid credentials'
+            ],401);
+        }else{
+            if($user->first_name!=null){
+                Auth::login($user);
+                
+                $token = $user->createToken('auth_token')->plainTextToken;
+                return response()->json([
+                     'is_new'=>false,
+                     'token'=> $token,
+                     'user' => $user
+                 ]);
+               }else{
+                return response()->json([
+                    'is_new' =>true
+                ],200);
+            }
+        }
+    }
+
+    public function destory(){
+        Auth::user()->currentAccessToken()->delete();
+    }
+}
---
+++ LaravelProgrammingLanguages/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rules\Isnumber;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+class RegisterController extends Controller
+{
+    public function store(){
+        if (Auth::check()) {
+            return response()->json(['message' => 'Access denied for authenticated users.'], 403);
+        }
+        request()->validate([
+            'number'=>['min:9' , 'max:10' ,new Isnumber()]  
+        ]);
+        $number = request('number');
+        if(strlen($number)==10){
+            $number= substr($number,1);
+        }
+        $number = '+963'. $number;
+        
+        $code = rand(1000,9999);
+        $params=array(
+            'token' => 'z3cxgqyxc5s3ztx9',
+            'to' => $number,
+            'body' => 'Your code is '. $code . ', please don\'t send it to anyone. This code will expire in 10 minutes'
+            );
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => "https://api.ultramsg.com/instance100696/messages/chat",
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => "",
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 30,
+              CURLOPT_SSL_VERIFYHOST => 0,
+              CURLOPT_SSL_VERIFYPEER => 0,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => "POST",
+              CURLOPT_POSTFIELDS => http_build_query($params),
+              CURLOPT_HTTPHEADER => array(
+                "content-type: application/x-www-form-urlencoded"
+              ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        
+        curl_close($curl);
+        $user =User::where('number','=',$number)->first();
+        if($user!=null){
+            $user->update([
+                'otp'=>$code,
+                'otp_timestamp' => Carbon::now()->addMinutes(10)   
+            ]);
+        }else{
+             User::create([
+                'number' => $number,
+                'otp'=> $code,
+                'otp_timestamp' => Carbon::now()->addMinutes(10),
+                'role'=>'user'
+            ]);
+     }
+    }
+
+    public function update(){
+        if (Auth::check()) {
+            return response()->json(['message' => 'Access denied for authenticated users.'], 403);
+        }
+        request()->validate([
+            'number' =>['min:9','max:10',new Isnumber()],
+            'first_name' => ['required'],
+            'last_name'=>['required'],
+            'location' => ['required'],
+        ]);
+        $number = request('number');
+        if(strlen($number)==10){
+            $number= substr($number,1);
+        }
+        $number = '+963'. $number;
+
+        $user =User::where('number','=',$number)->first();
+        if(!$user){
+            return response()->json([
+                'verified'=>false,
+                'time'=>false,
+                'message' => 'Number is not correct'
+            ],400);
+        }
+        if($user->otp != request('otp')){
+            return response()->json([
+                'verified'=>false,
+                'time'=>true,
+                'message' => 'Invalid credentials'
+            ],401);
+        }
+
+
+
+
+        $user->update([
+            'first_name'=>request('first_name'),
+            'last_name'=>request('last_name'),
+            'location'=>request('location'),
+        ]);
+
+        Auth::login($user);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        if(!Auth::guest()){
+            echo "hello";
+        }
+        return response()->json([
+            'token'=> $token,
+            'user' => $user
+        ]);
+    }
+
+
+}
