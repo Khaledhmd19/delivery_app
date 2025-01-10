7f39c4d4a7c8b220c0ee2f45eb6be08f83b671bd
Khaled Hammoud <khaledmasoud.hammoud@gmail.com>
2025-01-10 11:40:28 +0300

    Add files via upload

---
+++ FlutterProgrammingLanguages/lib/services/auth_service.dart
@@ -0,0 +1,44 @@
+import 'package:delivery_app/models/user_model.dart';
+import 'package:dio/dio.dart';
+
+class AuthService {
+  Dio dio = Dio();
+
+  Future<bool> logInPhoneNumber(User user) async {
+    try {
+      var response = await dio.post(
+          'http://192.168.137.151:8000/api/otp-request',
+          data: user.toMapPhonenumber());
+
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<bool> logInOtp(User user) async {
+    try {
+      var response = await dio.post(
+          'http://192.168.137.151:8000/api/otp-verification',
+          data: user.toMapOtp());
+
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<bool> AccountDetail(User user) async {
+    try {
+      var response = await dio.post('http://192.168.137.151:8000/api/register',
+          data: user.toMapPhonenumber());
+
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+}
